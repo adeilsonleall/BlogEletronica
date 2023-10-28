ec13310401ef3a912860d55dd4b7220ce170be36
Adeilson Leal <ad.eletronica.al@gmail.com>
2023-10-28 04:31:42 -0300

    28.10.23 - 4h31

--- controle.js
+++ controle.js
@@ -1,7 +1,14 @@
 const imgMenu = document.getElementById('img-menu');
 const mainMenu = document.getElementById('main-menu');
+const pathImgClose = '/img/fechar.png';
+const pathImgMenu = '/img/menu.png';
+const menuOpen = false;
 
 imgMenu.addEventListener('click', function(){
-    console.log(mainMenu);
     mainMenu.classList.toggle('hidden');
+    if(mainMenu.classList.value==='hidden'){
+        imgMenu.setAttribute('src',pathImgMenu);
+    }else{
+        imgMenu.setAttribute('src',pathImgClose);
+    }    
 });

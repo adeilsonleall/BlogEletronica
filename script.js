0e00f385a376636b4510b9048608c669ffe1e0a1
Adeilson Leal <ad.eletronica.al@gmail.com>
2023-11-20 21:00:08 -0300

    update-20.11.23-21h

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const imgMenu = document.querySelector('#imgMenu');
 const barMenu = document.querySelector('.bar-menu');
+const imgLogo = document.querySelector('#img-logo');
 
-const togglekMenu = function(){
-    barMenu.classList.toggle('toggle-menu');
-}
+
+
+// Toggle Menu
+imgMenu.addEventListener('click',()=>{
+    barMenu.classList.toggle('hide-menu'); 
+    if(barMenu.classList.value === 'bar-menu hide-menu'){
+        imgMenu.classList.remove('rotate-scale-up');
+        imgMenu.classList.add('rotate-scale-down');
+        imgMenu.setAttribute('src','img/menu.png')
+    }else{
+        imgMenu.setAttribute('src','img/fechar.png');
+        imgMenu.classList.remove('rotate-scale-down');
+        imgMenu.classList.add('rotate-scale-up');
+    }   
+});
+
+// Logo anime
+setInterval(()=>{imgLogo.classList.toggle('pulse');},1500);
